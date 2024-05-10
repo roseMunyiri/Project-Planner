a7405bd517196baa23e8c700f63c798079d2a2a4
roseMUnyiri <roselynecuka@gmail.com>
2024-05-10 15:11:27 +0300

    register tasks in admin.py

--- Project/admin.py
+++ Project/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Task
+from Account.models import User
 
 admin.site.register(Project)
+admin.site.register(Task)
+
+
